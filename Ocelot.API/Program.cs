2f27284c1275bcafddf164aa83d71e5595e3fa27
Tiago Pedro Jr <tpj.tiago@gmail.com>
2024-08-10 22:50:21 -0300

    Add project files.

---
+++ Ocelot.API/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Configuration.AddJsonFile("configuration.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UsePathBase("/gateway");
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseOcelot().Wait();
+app.Run();
